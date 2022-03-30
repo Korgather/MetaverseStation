e0fc219d79663f7b5e2d5946b95acda264bd45d9
Eungwang <eungwang1203@gmail.com>
2022-03-31 01:37:41 +0900

    Docs user.ts -> 불필요한 코드 정리

--- front/src/actions/user.ts
+++ front/src/actions/user.ts
@@ -12,27 +12,12 @@ import faker from "faker";
 //   introduce: faker.lorem.paragraph(),
 // };
 
-const delay = (time: number, value?: any) =>
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      // resolve(value);
-      resolve("test");
-    }, time);
-  });
-
-// export const logIn = createAsyncThunk("user/logIn", async (data, thunkAPI) => {
-//   const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/users`);
-//   localStorage.setItem("me", JSON.stringify(res.data));
-//   return res.data.body.user;
-// });
-
 export const logOut = createAsyncThunk("user/logOut", async () => {
   localStorage.setItem("me", "");
 });
 
 export const loadMyInfo = createAsyncThunk("user/loadMyInfo", async () => {
   const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/users`);
-  // const data = JSON.parse(localStorage.getItem("me") as string);
   console.log(res.data);
   return res.data;
 });
