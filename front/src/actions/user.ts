adf8c48d26d8cdf14e2848d0a4ed63fcac3656de
Eungwang <eungwang1203@gmail.com>
2022-04-02 01:43:15 +0900

    ADD 닉네임변경 Action

--- front/src/actions/user.ts
+++ front/src/actions/user.ts
@@ -1,6 +1,7 @@
-import { IUser } from '@customTypes/user';
+import { IUser, IUserState } from '@customTypes/user';
 import { generateDummyPost } from '@lib/generateDummyData';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import userSlice, { saveAccessToken } from '@slices/userSlice';
 import axios from 'axios';
 import faker from 'faker';
 
@@ -16,7 +17,34 @@ export const logOut = createAsyncThunk('user/logOut', async () => {
   localStorage.setItem('me', '');
 });
 
-export const loadMyInfo = createAsyncThunk('user/loadMyInfo', async () => {
-  const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/users`);
+export const loadMyInfo = createAsyncThunk('user/loadMyInfo', async (data, thunkAPI) => {
+  const {
+    userSlice: { AccessToken },
+  } = thunkAPI.getState() as { userSlice: IUserState };
+  const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/users`, {
+    headers: {
+      Authorization: `Bearer ${AccessToken}}`,
+    },
+  });
   return res.data;
 });
+
+export const changeNickName = createAsyncThunk(
+  'user/changeNickName',
+  async (data: string, thunkAPI) => {
+    const {
+      userSlice: { AccessToken },
+    } = thunkAPI.getState() as { userSlice: IUserState };
+    const res = await axios.put(`${process.env.NEXT_PUBLIC_API_URL}/users/username`, data, {
+      params: {
+        userName: data,
+      },
+      headers: {
+        Authorization: `Bearer ${AccessToken}`,
+      },
+    });
+
+    console.log(res);
+    return data;
+  },
+);
