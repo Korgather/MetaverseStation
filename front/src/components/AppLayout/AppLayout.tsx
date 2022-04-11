d907d41b85a3146587d9eaf91ebd49721d712bbe
Eungwang <eungwang1203@gmail.com>
2022-04-11 13:44:57 +0900

    fetch 검색등록

--- front/src/components/AppLayout/AppLayout.tsx
+++ front/src/components/AppLayout/AppLayout.tsx
@@ -37,6 +37,15 @@ const AppLayout = ({ children }: AuxProps) => {
   return (
     <>
       <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-N4NRZP8');`,
+          }}
+        />
         <title>모두의 메타버스 - MoMe</title>
         <meta
           name="description"
