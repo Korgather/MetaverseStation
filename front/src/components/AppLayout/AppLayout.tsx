4e77220a8f48989b183facaf6c904751f21cdc3c
Eungwang <eungwang1203@gmail.com>
2022-04-12 18:19:24 +0900

    next-script 수정

--- front/src/components/AppLayout/AppLayout.tsx
+++ front/src/components/AppLayout/AppLayout.tsx
@@ -11,6 +11,7 @@ import WriteModal from '@components/writeModal/WriteModal';
 import DetailModal from '@components/detailModal/DetailModal';
 import FeedBack from './FeedBack';
 import Head from 'next/head';
+import Script from 'next/script';
 import shortid from 'shortid';
 
 interface AuxProps {
@@ -37,15 +38,6 @@ const AppLayout = ({ children }: AuxProps) => {
   return (
     <>
       <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                  'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
-                  })(window,document,'script','dataLayer','GTM-N4NRZP8');`,
-          }}
-        />
         <title>모두의 메타버스 - MoMe</title>
         <meta
           name="description"
@@ -68,6 +60,17 @@ const AppLayout = ({ children }: AuxProps) => {
         <meta key={shortid.generate()} property="og:locale" content="ko_KR" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-N4NRZP8');`,
+          }}
+        />
+      </Script>
       {updateModalState && <WriteModal />}
       {detailModalState && <DetailModal />}
       <LayoutWrapper className="layout">
