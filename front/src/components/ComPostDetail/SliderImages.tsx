828c09897574c94915781beb9915795e88e52100
Eungwang <eungwang1203@gmail.com>
2022-04-20 21:07:49 +0900

    styled

--- front/src/components/ComPostDetail/SliderImages.tsx
+++ front/src/components/ComPostDetail/SliderImages.tsx
@@ -10,46 +10,49 @@ const SliderImages = () => {
   const postDetail = useAppSelector((state) => state.communitySlice.comPostDetail);
   const me = useAppSelector((state) => state.userSlice.me);
   return (
-    <SliderFrame>
-      {postDetail?.imageList &&
-        postDetail?.imageList.map((item) => (
-          <StyledImages key={shortid.generate()}>
-            <Image
-              key={item.imagePath}
-              src={process.env.NEXT_PUBLIC_IMG_URL + item.imagePath}
-              alt=""
-              style={{ display: 'block' }}
-            />
-            {me ? (
-              <StyledA href={postDetail?.link} target="_blank">
-                <EntnerButton type="primary" htmlType="button">
-                  <div>입장하기</div>
-                </EntnerButton>
-              </StyledA>
-            ) : (
-              <Link href={'/login'}>
-                <EntnerButton type="primary" htmlType="button">
-                  <div>입장하기</div>
-                </EntnerButton>
-              </Link>
-            )}
-          </StyledImages>
-        ))}
-    </SliderFrame>
+    <>
+      <SliderFrame>
+        {postDetail?.imageList &&
+          postDetail?.imageList.map((item) => (
+            <StyledImages key={shortid.generate()}>
+              <Image
+                key={item.imagePath}
+                src={process.env.NEXT_PUBLIC_IMG_URL + item.imagePath}
+                alt=""
+                style={{ display: 'block' }}
+              />
+            </StyledImages>
+          ))}
+      </SliderFrame>
+      {me ? (
+        <StyledA href={postDetail?.link} target="_blank">
+          <EntnerButton type="primary" htmlType="button">
+            <div>입장하기</div>
+          </EntnerButton>
+        </StyledA>
+      ) : (
+        <Link href={'/login'}>
+          <EntnerButton type="primary" htmlType="button">
+            <div>입장하기</div>
+          </EntnerButton>
+        </Link>
+      )}
+    </>
   );
 };
 
 export default SliderImages;
 
 export const StyledA = styled.a`
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  z-index: 900;
+  display: flex;
+  width: 100%;
 `;
 
 export const EntnerButton = styled(Button)`
-  border-radius: 5px;
+  width: 100%;
+  height: 40px;
+  font-size: 1rem;
+  font-weight: 600;
 `;
 
 export const StyledImages = styled.div`
