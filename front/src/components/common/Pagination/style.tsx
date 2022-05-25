c3d3259c85c5ad49291120eb00130d48fb6a1ac6
Eungwang <eungwang1203@gmail.com>
2022-05-25 21:36:27 +0900

    refactor : Pagination컴포넌트 businesslogic, viewlogic 분리

---
+++ front/src/components/common/Pagination/style.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const PaginationWrapper = styled.div`
+  width: 1440px;
+  text-align: center;
+  padding-top: 50px;
+  @media screen and (max-width: 1650px) {
+    width: 75vw;
+  }
+`;
