0f8895f118d3e9811c04d032daa2a6c7b7817747
Eungwang <eungwang1203@gmail.com>
2022-05-29 12:47:58 +0900

    fetch : channelIfram의 경우 디스코드 위젯 언로드

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -9,12 +9,15 @@ import 'react-quill/dist/quill.snow.css';
 import 'slick-carousel/slick/slick-theme.css';
 import AppInner from '@components/AppInner';
 import { CookiesProvider } from 'react-cookie';
+import { useRouter } from 'next/router';
 declare global {
   interface Window {
     Kakao: any;
   }
 }
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const isChannelIframe = router.pathname === '/game/channelIframe';
   useEffect(() => {
     async function loadCrate() {
       const result = await import('@widgetbot/crate');
@@ -25,7 +28,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       });
       crate.notify('모두메타 채팅방');
     }
-    loadCrate();
+    !isChannelIframe && loadCrate();
     window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY);
   }, []);
   return (
