8ae569a76191edf41921ee8644c03b5587820783
Eungwang <eungwang1203@gmail.com>
2022-05-31 21:12:11 +0900

    fetch : 모바일환경에서 게시글 클릭시 모달을 띄우는 형태에서 페이지 이동의 형태로 변경

--- front/src/components/common/Postzone/PostZoneContainer.tsx
+++ front/src/components/common/Postzone/PostZoneContainer.tsx
@@ -4,6 +4,8 @@ import { useAppDispatch } from '@store/hook';
 import { loadPost, viewPost } from '@actions/post';
 import { ToggleDetailState } from '@slices/postSlice';
 import PostZoneView from './PostZoneView';
+import { useRouter } from 'next/router';
+import { useMedia } from '@lib/useMedia';
 
 export interface GameImages {
   src: string;
@@ -21,19 +23,27 @@ const Postzone: React.FunctionComponent<PostzoneProps> = ({
   Images = null,
   imageHeight = '15.625rem',
 }) => {
+  const router = useRouter();
   const dispatch = useAppDispatch();
   const loadPostId = async (data: IPost) => {
     await Promise.all([dispatch(viewPost(data.id)), dispatch(loadPost(data.id))]);
   };
-  const onLoadPost = (post: IPost) => {
-    post && loadPostId(post);
-    dispatch(ToggleDetailState(true));
+  const { isMobile } = useMedia();
+  const openDetailModal = (post: IPost) => {
+    if (!isMobile) {
+      post && loadPostId(post);
+      dispatch(ToggleDetailState(true));
+    }
+  };
+  const gotoDetail = (post: IPost) => {
+    router.push(`/community/post/${post.id}`);
   };
   const PostzoneProps = {
     mainPosts,
-    onLoadPost,
+    openDetailModal,
     Images,
     imageHeight,
+    gotoDetail,
   };
   return <PostZoneView {...PostzoneProps} />;
 };
