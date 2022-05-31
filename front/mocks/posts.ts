a44ad38248d52bc8f15eba49923cc518c51b3a29
Eungwang <eungwang1203@gmail.com>
2022-05-31 10:46:45 +0900

    test : test를 위한 mockdata추가

---
+++ front/mocks/posts.ts
@@ -0,0 +1,206 @@
+import { IPost } from '@customTypes/post';
+
+export const mockPosts: IPost[] = [
+  {
+    id: 199,
+    category: 'METAVERSE_GATHERTOWN',
+    postUser: {
+      userId: 70,
+      username: '얼웨이즈',
+      profileImageUrl:
+        'http://k.kakaocdn.net/dn/cmULHg/btraesVla6Q/0rKhbK91KA3D9EtIQtHuxk/img_110x110.jpg',
+      email: 'NO_EMAIL',
+      roleType: 'USER',
+      bio: null,
+    },
+    title: '외국맵 Diverse-City',
+    content:
+      '<p>꾸준한 사랑을 받는 외국 맵 <strong>Diverse-City</strong> 맵 입니다.</p><p><br></p><p>커뮤니티 목적의 맵입니다.</p><p><br></p><p><br></p>',
+    view: 168,
+    imageList: [
+      {
+        origFileName: '화면 캡처 2022-04-24 174824.png',
+        imagePath: '',
+        fileSize: 289794,
+      },
+      {
+        origFileName: '화면 캡처 2022-04-24 174915.png',
+        imagePath: '',
+        fileSize: 121542,
+      },
+      {
+        origFileName: '화면 캡처 2022-04-24 174952.png',
+        imagePath: '',
+        fileSize: 116616,
+      },
+    ],
+    link: 'https://app.gather.town/app/VCiCJMip5IiyYCG3/Malvius%20Cave',
+    postCommentList: [],
+    likeUserList: {
+      '1': 'Gwang',
+    },
+    playerCount: 20,
+    createdDate: '2022-04-24T17:51:47.986562',
+  },
+  {
+    id: 164,
+    category: 'METAVERSE_GATHERTOWN',
+    postUser: {
+      userId: 1,
+      username: 'Gwang',
+      profileImageUrl:
+        'https://cdn.metabusstation.shop/profileImage/197d5525-3001-4675-afba-914c17c65d8dKakaoTalk_20220413_001825763.png',
+      email: 'eungwang1203@gmail.com',
+      roleType: 'USER',
+      bio: '안녕하세요.\n게더타운 한국 커뮤니티 코게더의 매니져 Gwang입니다.',
+    },
+    title: '🥤코카콜라 게더타운 맵 - 원더플 아일랜드!',
+    content:
+      '<h3><strong style="background-color: rgb(243, 244, 244); color: rgb(34, 34, 34);">코카콜라, 메타버스 플랫폼 속 ‘원더플 아일랜드’로 자원순환 체험한다.</strong></h3><p>코카콜라에서 제작한 메타버스공간입니다.</p><p>음료 페트병의 자원순환을 가상에서 체험해 볼 수 있는 공간이며,</p><p>무엇보다 너무 예쁘게 꾸며져 있어서 구경하는 재미가 있습니다.</p>',
+    view: 453,
+    imageList: [
+      {
+        origFileName: '20220414000259.png',
+        imagePath: '',
+        fileSize: 1044177,
+      },
+      {
+        origFileName: '234.png',
+        imagePath: '',
+        fileSize: 567317,
+      },
+    ],
+    link: 'https://app.gather.town/app/cDRgrckO2OfoZSJc/onethepl',
+    postCommentList: [
+      {
+        commentId: 225,
+        userId: 3,
+        username: '메타',
+        content: '맵 너무 예쁘네요 !',
+        profileImageUrl:
+          'https://cdn.metabusstation.shop/profileImage/38788d96-d20a-480a-9062-97417731db31Logo01.png',
+        postCommentReplyList: [],
+        createdDate: '2022-04-14T01:44:51.785689',
+      },
+    ],
+    likeUserList: {
+      '1': 'Gwang',
+      '50': '하늘연달',
+      '2': 'username-',
+      '3': '메타',
+      '5': '얼그레이',
+      '40': 'won',
+    },
+    playerCount: 1,
+    createdDate: '2022-04-14T00:05:55.403239',
+  },
+  {
+    id: 176,
+    category: 'METAVERSE_GATHERTOWN',
+    postUser: {
+      userId: 1,
+      username: 'Gwang',
+      profileImageUrl:
+        'https://cdn.metabusstation.shop/profileImage/197d5525-3001-4675-afba-914c17c65d8dKakaoTalk_20220413_001825763.png',
+      email: 'eungwang1203@gmail.com',
+      roleType: 'USER',
+      bio: '안녕하세요.\n게더타운 한국 커뮤니티 코게더의 매니져 Gwang입니다.',
+    },
+    title: 'Web-flower',
+    content:
+      '<h3>Web-flower</h3><p>이전에 Gathertown Explore에 등록되어 있었던 Web-flower라는 맵인데요,</p><p>맵 퀄리티가 너무 좋아서 올려봅니다.</p><p>업무공간부터, 세미나공간, 홍보공간까지 정말 많은 공간들이 있구요 .. !</p><p>너무 광범위해서 사진으로 담기에는 어려움이 있네요 ! 한번 직접 들어가서</p><p>구경해 보시면 좋을 것 같습니다 😊😊</p>',
+    view: 332,
+    imageList: [
+      {
+        origFileName: '20220415144805.png',
+        imagePath: '',
+        fileSize: 237053,
+      },
+      {
+        origFileName: '20220415144613.png',
+        imagePath: '',
+        fileSize: 210976,
+      },
+      {
+        origFileName: '20220415144733.png',
+        imagePath: '',
+        fileSize: 360853,
+      },
+      {
+        origFileName: '20220415144704.png',
+        imagePath: '',
+        fileSize: 213942,
+      },
+    ],
+    link: 'https://app.gather.town/app/r56yyuDY2VbHTxxb/Web-flowers',
+    postCommentList: [
+      {
+        commentId: 236,
+        userId: 51,
+        username: '새우새우',
+        content: '우와!!! 이런 방대한 맵이있다니!',
+        profileImageUrl:
+          'http://k.kakaocdn.net/dn/dwl9T8/btrsJLikpZW/uU1vqbytCzBPNZWFj7g1F1/img_110x110.jpg',
+        postCommentReplyList: [],
+        createdDate: '2022-04-17T23:46:20.215011',
+      },
+    ],
+    likeUserList: {
+      '1': 'Gwang',
+      '50': '하늘연달',
+      '3': '메타',
+      '51': '새우새우',
+      '70': '얼웨이즈',
+    },
+    playerCount: 1,
+    createdDate: '2022-04-15T14:51:50.917542',
+  },
+  {
+    id: 208,
+    category: 'METAVERSE_ZEP',
+    postUser: {
+      userId: 1,
+      username: 'Gwang',
+      profileImageUrl:
+        'https://cdn.metabusstation.shop/profileImage/197d5525-3001-4675-afba-914c17c65d8dKakaoTalk_20220413_001825763.png',
+      email: 'eungwang1203@gmail.com',
+      roleType: 'USER',
+      bio: '안녕하세요.\n게더타운 한국 커뮤니티 코게더의 매니져 Gwang입니다.',
+    },
+    title: '💣 젭 6인 마피아게임 ! ',
+    content:
+      '<p>안녕하세요.&nbsp;</p><p>젭스크립트를 이용해서</p><p>6인이 즐길 수 있는 마피아게임을 만들어봤습니다.</p><p>6인을 모으기는 쉽지 않겠지만.. ! 체험해 보고 싶으신 분들은 입장하기를 눌러 게임에 입장할 수 있습니다.&nbsp;</p><p>입장 후, 채팅창에 "참가"를 입력해 게임을 시작할 수 있습니다. 6명의 인원이 "참가"를 입력하면 게임이 자동으로 시작됩니다.</p><p>부족하지만 열심히 만들어봤습니다 :D</p><p>체험해보시고 후기 남겨주시면 감사하겠습니다 !!&nbsp;</p><p><br></p><p>[ 승리 조건 ]</p><p><strong>마피아</strong>가 죽으면 <strong>시민</strong>이 승리합니다.</p><p><strong>마피아</strong>와 <strong>시민</strong>의 수가 같으면 <strong>마피아</strong>가 승리합니다.</p><p><br></p><p>[ 게임 룰 ]</p><p>마피아 1명, 경찰1명, 의사1명, 시민 3명으로 게임이 시작됩니다.</p><p>매일 아침 투표를 통해, 플레이어를 처형 할 수 있습니다. 단, 득표수가 동일 할 경우 아무도 죽지 않습니다.</p><p><br></p><p>[직업 능력]</p><p>마피아 : 마피아는 밤마다 플레이어를 한명 죽일 수 있습니다.</p><p>의사 : 의사는 밤마다 마피아로 부터 플레이어를 한명 구할 수 있습니다.</p><p>경찰 : 경찰은 밤마다 플레이어 한명의 직업을 조사할 수 있습니다.</p><p><br></p><p>[직업 공통] - 시민 제외</p><p>직업을 가진 플레이어는 밤에 다른 플레이어에게 보이지 않습니다.</p><p>대상 플레이어 앞에서 Z를 눌러 능력을 사용 할 수 있습니다.</p><p><br></p><p>※ 맵을 복제해 두개의 채널을 만들어 놨습니다.</p><p>1채널 : <a href="https://zep.us/play/yV9AA9" rel="noopener noreferrer" target="_blank">https://zep.us/play/yV9AA9</a></p><p>2채널 : <a href="https://zep.us/play/8g1o9b" rel="noopener noreferrer" target="_blank">https://zep.us/play/8g1o9b</a></p>',
+    view: 298,
+    imageList: [
+      {
+        origFileName: 'KakaoTalk_20220524_001119619.png',
+        imagePath: '',
+        fileSize: 669313,
+      },
+      {
+        origFileName: '화면 캡처 2022-05-23 170424.png',
+        imagePath: '',
+        fileSize: 166490,
+      },
+      {
+        origFileName: 'KakaoTalk_20220523_170526424.png',
+        imagePath: '',
+        fileSize: 96692,
+      },
+      {
+        origFileName: 'KakaoTalk_20220523_170526053.png',
+        imagePath: '',
+        fileSize: 210402,
+      },
+    ],
+    link: 'https://www.modumeta.com/game',
+    postCommentList: [],
+    likeUserList: {
+      '1': 'Gwang',
+      '70': '얼웨이즈',
+      '8': '디코',
+    },
+    playerCount: 1,
+    createdDate: '2022-05-23T17:18:28.266546',
+  },
+];
