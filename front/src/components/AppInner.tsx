ed8b9cc035af9aaa3d5308a35c1f130679d6e344
Eungwang <eungwang1203@gmail.com>
2022-06-06 10:43:28 +0900

    style : 불필요한 서버사이드 코드 삭제

--- front/src/components/AppInner.tsx
+++ front/src/components/AppInner.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import { NextComponentType, NextPageContext } from 'next';
-import wrapper from '@store/configureStore';
-import axios from 'axios';
-import cookies from 'next-cookies';
-import { logOut, saveAccessToken } from '@slices/userSlice';
-import { loadMyInfo } from '@actions/user';
 
 type Props = {
   pageProps: any;
@@ -16,14 +11,3 @@ const AppInner = ({ pageProps, Component }: Props) => {
 };
 
 export default AppInner;
-export const getServerSideProps = wrapper.getServerSideProps((store) => async (ctx) => {
-  store.dispatch(logOut());
-  axios.defaults.headers.common['Authorization'] = '';
-  const token = cookies(ctx).Token;
-  if (ctx.req && token) {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    store.dispatch(saveAccessToken(token));
-    await store.dispatch(loadMyInfo());
-  }
-  return { props: {} };
-});
--- front/src/components/login/LoginContainer.tsx
+++ front/src/components/login/LoginContainer.tsx
@@ -1,10 +1,5 @@
-import { loadMyInfo } from '@actions/user';
 import { IUser } from '@customTypes/user';
-import { logOut, saveAccessToken } from '@slices/userSlice';
-import wrapper from '@store/configureStore';
 import { useAppSelector } from '@store/hook';
-import axios from 'axios';
-import cookies from 'next-cookies';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
 import LoginView from './LoginView';
@@ -42,15 +37,3 @@ const LoginContainer = () => {
 };
 
 export default LoginContainer;
-
-export const getServerSideProps = wrapper.getServerSideProps((store) => async (ctx) => {
-  store.dispatch(logOut());
-  axios.defaults.headers.common['Authorization'] = '';
-  const token = cookies(ctx).Token;
-  if (ctx.req && token) {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    store.dispatch(saveAccessToken(token));
-    await store.dispatch(loadMyInfo());
-  }
-  return { props: {} };
-});
