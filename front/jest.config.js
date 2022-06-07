6b7546dc015963d69e1d5425327aba0a76d3f5e1
Eungwang <eungwang1203@gmail.com>
2022-06-07 22:30:14 +0900

    fetch : test를 위한 환경변수 설정

--- front/jest.config.js
+++ front/jest.config.js
@@ -8,6 +8,7 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
+  globalSetup: '<rootDir>/__test__/setupEnv.js',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
