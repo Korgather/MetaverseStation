499025908fbb9645fb8f8c371c3b7128c18214a4
Eungwang <eungwang1203@gmail.com>
2022-06-07 12:46:23 +0900

    docs : bundle analyzer설정 및 styledComponents swc방식으로 변경

--- front/next.config.js
+++ front/next.config.js
@@ -1,9 +1,32 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 const nextConfig = {
+  compress: true,
   reactStrictMode: true,
   images: {
     domains: ['cdn.metabusstation.shop'],
   },
+  swcMinify: true,
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
+  webpack(config, { webpack }) {
+    const prod = process.env.NODE_ENV === 'production';
+    const plugins = [
+      ...config.plugins,
+      new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
+    ];
+    return {
+      ...config,
+      mode: prod ? 'production' : 'development',
+      devtool: prod ? 'hidden-source-map' : 'eval',
+      plugins,
+    };
+  },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
