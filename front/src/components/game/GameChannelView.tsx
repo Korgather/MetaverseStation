814d2372826783a5a3b23b702bc5f09c93a4ceb1
Eungwang <eungwang1203@gmail.com>
2022-06-14 01:32:06 +0900

    fetch : GameChannelView Link -> router로 변경

--- front/src/components/game/GameChannelView.tsx
+++ front/src/components/game/GameChannelView.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 import styled from 'styled-components';
 import GameView from './GameView';
 import CurrentUser from '@components/common/Postzone/CurrentUser';
-import Link from 'next/link';
 import { IChannelImages } from './GameContainer';
+import { useRouter } from 'next/router';
 
 interface GameChannel {
   bannerImage: string[];
@@ -16,23 +16,29 @@ interface GameChannel {
 }
 
 const GameChannel = ({ channelImages, isMatch }: GameView) => {
+  const router = useRouter();
+  const onChangeChannel = (url: string) => {
+    router.push(url);
+  };
   return (
     <Wrapper>
       {(isMatch.mapia ? channelImages.mapia : channelImages.omok).map((image) => (
-        <Link href={image.url} key={image.src}>
-          <div style={{ position: 'relative', cursor: 'pointer' }}>
-            <Image
-              src={image.src}
-              width="100%"
-              height="50px"
-              layout="responsive"
-              objectFit="cover"
-              alt="게임채널이미지"
-              quality={10}
-            />
-            <CurrentUser image={image} />
-          </div>
-        </Link>
+        <div
+          style={{ position: 'relative', cursor: 'pointer' }}
+          key={image.src}
+          onClick={() => onChangeChannel(image.url)}
+        >
+          <Image
+            src={image.src}
+            width="100%"
+            height="50px"
+            layout="responsive"
+            objectFit="cover"
+            alt="게임채널이미지"
+            quality={10}
+          />
+          <CurrentUser image={image} />
+        </div>
       ))}
     </Wrapper>
   );
