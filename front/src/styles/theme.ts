b77a053d1f8fb6d48fa8450c00d52a2ecc82e9c3
심은광 <scm323@naver.com>
2022-07-15 11:04:25 +0900

    style : 공용skeleton css 추가

--- front/src/styles/theme.ts
+++ front/src/styles/theme.ts
@@ -1,3 +1,5 @@
+import styled, { css } from 'styled-components';
+
 const customMediaQuery = (maxWidth: number): string => `@media (max-width: ${maxWidth}px)`;
 
 export const media = {
@@ -5,3 +7,26 @@ export const media = {
   tablet: customMediaQuery(1024),
   mobile: customMediaQuery(850),
 };
+
+export const skeletonCss = css`
+  position: relative;
+  background-color: #ecf0f1;
+  ::after {
+    display: block;
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    transform: translateX(-100%);
+    background: linear-gradient(90deg, transparent, rgba(255, 255, 255, 0.5), transparent);
+    animation: loading 1.5s infinite;
+  }
+`;
+
+export const skeletonKeyframes = css`
+  @keyframes loading {
+    100% {
+      transform: translateX(100%);
+    }
+  }
+`;
