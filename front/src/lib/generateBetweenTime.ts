f53ee1225b591aebb703f3e572e5d4a8711aee3f
Eungwang <86244477+eungwang1@users.noreply.github.com>
2023-01-20 15:07:54 +0900

    Fix: 데이터형식 에러 해결

--- front/src/lib/generateBetweenTime.ts
+++ front/src/lib/generateBetweenTime.ts
@@ -3,6 +3,9 @@ import { IPost } from '@customTypes/post';
 
 export const generateBetweenTime = (comment: IComment | IReply | IPost) => {
   if (comment.createdDate && comment.createdDate.length >= 5) {
-    return `${comment.createdDate[0]}-${comment.createdDate[1]}-${comment.createdDate[2]} ${comment.createdDate[3]}:${comment.createdDate[4]}`;
+    const FormatedCreatedDate = comment.createdDate.map((el) =>
+      String(el).padEnd(2, '0')
+    );
+    return `${FormatedCreatedDate[0]}-${FormatedCreatedDate[1]}-${FormatedCreatedDate[2]} ${FormatedCreatedDate[3]}:${FormatedCreatedDate[4]}`;
   }
 };
